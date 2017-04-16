2dd96ea4da30f65132cfd1ed060d82c489187362
zavarnik1 <zavarnik1@gmail.com>
2017-04-16 22:06:21 +0300

    HW3 resolve

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -34,3 +34,13 @@ CREATE TABLE meals (
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals(user_id, date_time)
+
+CREATE TABLE meals (
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id     INTEGER NOT NULL,
+  date_time    TIMESTAMP NOT NULL,
+  description TEXT NOT NULL,
+  calories    INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals(user_id, date_time)
